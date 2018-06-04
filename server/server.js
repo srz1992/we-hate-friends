fc0afdea7f124341bb4b9fb407158fbf0c40a94a
sean zilka <zilka.sr@gmail.com>
2018-06-04 17:56:59 -0500

    created basic file structure

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
+const messageRouter = require('./routes/router')
+
+app.use(express.static('server/public'))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+app.use('/message', messageRouter);
+
+const DB_NAME = 'list';
+const DB_URL = process.env.MONGODB_URL || `mongodb://localhost:27017/${DB_NAME}`
+mongoose.connect(DB_URL);
+
+mongoose.connection.on('connected', ()=>{
+    console.log('mongoose is connected to ', DB_URL);  
+})
+
+mongoose.connection.on('error', (error)=>{
+    console.log('mongoose connection error:', error);  
+})
+
+app.listen(port, ()=>{
+    console.log('listening on port:', port); 
+})
